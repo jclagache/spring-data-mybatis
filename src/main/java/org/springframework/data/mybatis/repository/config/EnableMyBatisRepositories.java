71ea889503f3e73c620e9204cb09f6904f6c6b2c
vitaly.pronto <vitaly.pronto@gmail.com>
2015-06-05 12:47:44 +0300

    API code cleaned

--- src/main/java/org/springframework/data/mybatis/repository/config/EnableMyBatisRepositories.java
+++ src/main/java/org/springframework/data/mybatis/repository/config/EnableMyBatisRepositories.java
@@ -17,7 +17,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
-@Import({MyBatisRepositoriesRegistrar.class})
+@Import({MyBatisRepositoriesRegistrar.class, InfrastructureConfig.class})
 public @interface EnableMyBatisRepositories {
 	/**
 	 * Alias for the {@link #basePackages()} attribute. Allows for more concise
--- src/test/java/org/springframework/data/mybatis/ApplicationConfig.java
+++ src/test/java/org/springframework/data/mybatis/ApplicationConfig.java
@@ -1,15 +1,12 @@
 package org.springframework.data.mybatis;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mybatis.repository.config.EnableMyBatisRepositories;
-import org.springframework.data.mybatis.repository.config.InfrastructureConfig;
 
 @Configuration
 @EnableMyBatisRepositories
 @EnableAutoConfiguration
-@ComponentScan(basePackageClasses = {InfrastructureConfig.class})
 public class ApplicationConfig {
 
 }
