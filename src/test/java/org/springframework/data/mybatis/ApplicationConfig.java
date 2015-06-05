282175bacf36dc1c44eeed5233710cf50dae732d
vitaly.pronto <vitaly.pronto@gmail.com>
2015-06-05 12:46:08 +0300

    test cleaned

--- src/test/java/org/springframework/data/mybatis/ApplicationConfig.java
+++ src/test/java/org/springframework/data/mybatis/ApplicationConfig.java
@@ -3,14 +3,12 @@ package org.springframework.data.mybatis;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.mybatis.repository.config.EnableMyBatisRepositories;
 import org.springframework.data.mybatis.repository.config.InfrastructureConfig;
 
 @Configuration
 @EnableMyBatisRepositories
 @EnableAutoConfiguration
-@Import({InfrastructureConfig.class})
 @ComponentScan(basePackageClasses = {InfrastructureConfig.class})
 public class ApplicationConfig {
 
